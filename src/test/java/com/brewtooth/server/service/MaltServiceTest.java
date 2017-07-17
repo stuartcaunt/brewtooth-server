8cf71a510a12533dbfe449e84f9692e87ce124e3
Stuart Caunt <caunt@cauntbook.gen.ill.fr>
2017-07-17 11:35:53 +0200

    Starting to get things running for tests.

--- src/test/java/com/brewtooth/server/service/MaltServiceTest.java
+++ src/test/java/com/brewtooth/server/service/MaltServiceTest.java
@@ -2,20 +2,24 @@ package com.brewtooth.server.service;
 
 import com.brewtooth.server.BrewToothConfiguration;
 import com.brewtooth.server.BrewToothServer;
+import com.brewtooth.server.domain.Malt;
 import com.brewtooth.server.persistence.BrewToothHibernateBundle;
 import com.brewtooth.server.persistence.BrewToothHibernateModule;
+import com.codahale.metrics.MetricRegistry;
+import com.google.inject.Inject;
 import com.hubspot.dropwizard.guice.GuiceBundle;
 import com.squarespace.jersey2.guice.JerseyGuiceUtils;
+import io.dropwizard.jackson.Jackson;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 
@@ -24,6 +28,9 @@ public class MaltServiceTest {
 
 	@ClassRule
 	public static final DropwizardAppRule<BrewToothConfiguration> RULE = new DropwizardAppRule<BrewToothConfiguration>(BrewToothServer.class, ResourceHelpers.resourceFilePath("brewtooth-server-test.yml"));
+
+	@Inject
+	private MaltService maltService;
 
 	@Before
 	public void setUp() throws Exception {
@@ -46,11 +53,13 @@ public class MaltServiceTest {
 
 		Bootstrap bootstrap = mock(Bootstrap.class);
 
-//		hibernateBundle.initialize(bootstrap);
+		Environment environment = new Environment("test env", Jackson.newObjectMapper(), null, new MetricRegistry(), null);
+
 		guiceBundle.initialize(bootstrap);
 
-//		hibernateBundle.run(RULE.getConfiguration(), RULE.getEnvironment());
-		guiceBundle.run(RULE.getConfiguration(), RULE.getEnvironment());
+		hibernateBundle.run(RULE.getConfiguration(), environment);
+		guiceBundle.run(RULE.getConfiguration(), environment);
+		guiceBundle.getInjector().injectMembers(this);
 	}
 
 	@After
@@ -62,8 +71,12 @@ public class MaltServiceTest {
 	@Test
 	@DisplayName("Test Service")
 	public void testService() {
+		Assert.assertNotNull(this.maltService);
 
-
+		List<Malt> malts = this.maltService.getAll();
+		Assert.assertEquals(0, malts.size());
 	}
+
+
 
 }
