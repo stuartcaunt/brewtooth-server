6d672434d008f2857eca96c1bfc8c60ee5d57389
Stuart Caunt <caunt@cauntbook.gen.ill.fr>
2017-07-21 08:31:00 +0200

    Use logback.xml to configure logging during tests.

--- src/test/java/com/brewtooth/server/service/MaltServiceTest.java
+++ src/test/java/com/brewtooth/server/service/MaltServiceTest.java
@@ -1,14 +1,9 @@
 package com.brewtooth.server.service;
 
-import com.brewtooth.server.BrewToothConfiguration;
-import com.brewtooth.server.BrewToothServer;
 import com.brewtooth.server.PersistenceExtension;
 import com.brewtooth.server.domain.Malt;
 import com.google.inject.Inject;
-import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.Assert;
-import org.junit.ClassRule;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
