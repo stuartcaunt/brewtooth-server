3227a6d31c5e3c85b1bad2e78875474066a7134c
Stuart Caunt <stuart.caunt@gmail.com>
2017-07-31 21:37:08 +0200

    Add CORS Filter configuration to server. Modify application context path and malts url.

--- src/main/java/com/brewtooth/server/BrewToothServer.java
+++ src/main/java/com/brewtooth/server/BrewToothServer.java
@@ -9,10 +9,12 @@ import com.hubspot.dropwizard.guice.GuiceBundle;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import java.util.EnumSet;
 import java.util.Properties;
 
@@ -53,6 +55,7 @@ public class BrewToothServer extends Application<BrewToothConfiguration> {
 			.setConfigClass(BrewToothConfiguration.class).build();
 
 		bootstrap.addBundle(guiceBundle);
+
 	}
 
 	@Override
@@ -61,6 +64,17 @@ public class BrewToothServer extends Application<BrewToothConfiguration> {
 
 		// !!! NEEDED !!! - otherwise the EntityManager is not correct for all the requests
 		environment.servlets().addFilter("persistFilter", guiceBundle.getInjector().getInstance(PersistFilter.class)).addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
+
+		// Enable CORS headers
+		final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+		// Configure CORS parameters
+		cors.setInitParameter("allowedOrigins", "*");
+		cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+		cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+
+		// Add URL mapping
+		cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
 		// Register resources
 		environment.jersey().register(guiceBundle.getInjector().getInstance(MaltEndpoint.class));
